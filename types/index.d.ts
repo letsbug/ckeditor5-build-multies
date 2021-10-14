7b58036842f18b20fc97d7ed9179812343dbfc01
letsbug <xman724939995@gmail.com>
2021-10-14 10:14:38 +0800

    fix types

--- types/index.d.ts
+++ types/index.d.ts
@@ -123,9 +123,9 @@ export class MceDecoupled extends MceBase {
 }
 
 declare const HlxMceBuilds: {
-	BuildInline: MceInline;
-	BuildClassic: MceClassic;
-	BuildDecoupled: MceDecoupled;
+	BuildInline: typeof MceInline;
+	BuildClassic: typeof MceClassic;
+	BuildDecoupled: typeof MceDecoupled;
 };
 
 export default HlxMceBuilds;
