7132f54b8d66bc56b4a44a19844b8902ffd68144
letsbug <letsbug@qq.com>
2022-04-13 10:18:52 +0800

    chore: 修改配置

--- .hlxrc.ts
+++ .hlxrc.ts
@@ -13,11 +13,4 @@ export default defineConfig({
 		user: process.env.HLX_NEXUS_USER,
 		token: process.env.HLX_NEXUS_TOKEN,
 	},
-	deploy: {
-		service: process.env.HLX_DOCKER_SERVICE as IpUrl,
-		registry: process.env.HLX_DOCKER_REGISTRY as IpUrl,
-		user: process.env.HLX_DOCKER_USER,
-		password: process.env.HLX_DOCKER_PWD,
-		email: process.env.HLX_DOCKER_EMAIL,
-	},
 });
